d7e728fa2548d57b08565f9f7050212c3d452399
Owen <1787hz@gmail.com>
2017-06-15 16:15:06 +0800

    bug fix line web

--- public/www/scripts/app.run.js
+++ public/www/scripts/app.run.js
@@ -1,7 +1,6 @@
 ﻿angular.module('2017Web').run(['$rootScope', function ($rootScope) {
     $rootScope.storeId = '24';
-    var domain = 'https://ilab.csie.io';
-    //var domain = 'http://192.168.50.86:3024';
+    var domain = 'https://ostore.herokuapp.com';
+    //var domain = 'http://127.0.0.1:3000';
     $rootScope.url = domain + '/apps' + $rootScope.storeId + '/store';
-    $rootScope.istoreUrl = 'https://ilab.csie.io/apps09/istore';
 }]);
