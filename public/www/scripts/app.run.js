38855cc16ddeea39f110e433d3ebf15c4a91ffe0
Owen <1787hz@gmail.com>
2017-06-15 14:40:08 +0800

    line web

--- public/www/scripts/app.run.js
+++ public/www/scripts/app.run.js
@@ -1,6 +1,7 @@
 ﻿angular.module('2017Web').run(['$rootScope', function ($rootScope) {
     $rootScope.storeId = '24';
-    var domain = 'https://ostore.herokuapp.com';
+    var domain = 'https://ilab.csie.io';
     //var domain = 'http://192.168.50.86:3024';
     $rootScope.url = domain + '/apps' + $rootScope.storeId + '/store';
+    $rootScope.istoreUrl = 'https://ilab.csie.io/apps09/istore';
 }]);
