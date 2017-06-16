c3ab4ebe1b1f14a9da7d9bc02086c285e16fb9a7
Owen <1787hz@gmail.com>
2017-06-16 10:41:35 +0800

    fix bot port

--- helpers/lineBot.js
+++ helpers/lineBot.js
@@ -1,5 +1,6 @@
 var lineSecret = require('../config/storeConfig').line.channelSecret,
     lineToken = require('../config/storeConfig').line.channelAccessToken,
+    botPort = require('../config/storeConfig').line.botPort,
     { storeId } = require('../config/storeConfig').store;
 
 var LineBot = require('node-line-messaging-api');
@@ -8,7 +9,7 @@ var lineBot = new LineBot({
     secret: lineSecret,
     token: lineToken,
     options: {
-        port: `31${storeId}`,
+        port: botPort,
         tunnel: false,
         verifySignature: true,
         endpoint: `/apps${storeId}/line/webhook`
