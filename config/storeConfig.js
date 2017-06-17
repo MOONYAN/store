24cecccf7d7e619083c113733e5a7faf1c407be0
Owen <1787hz@gmail.com>
2017-06-17 22:33:40 +0800

    generate env file

--- config/storeConfig.js
+++ config/storeConfig.js
@@ -13,15 +13,15 @@ module.exports = {
     store: {
         storeId: storeId,
         storeName: `Store ${storeId}`,
-        storeDB: process.env.MONGODB_URI || `mongodb://Naruto:12345678@ds125262.mlab.com:25262/heroku_mqvq7nsb`,
+        storeDB: process.env.MONGODB_URI,
         storeSecret: `store${storeId}`,
         storePath: `/apps${storeId}/store`,
         storeTopic: `/topics/store${storeId}`
     },
 
     line: {
-        channelSecret: '6cdcbb477e3d9f673b4135e44bcfc3c0',
-        channelAccessToken: 'RVjSrE/1HODqVk/m6CH9lYJ35IMzLnZSDx7oB7q7hO0s4Zb3Xt2g+TEuObmXvbDMqzwtEeAVcrHTOFkUtY+eHqBtYgPEKC7TR7ZnUmjgFA6i/Zx37/iorOg3l6DBQWNdPCWztLTyW7/pNtvvZqD/bAdB04t89/1O/w1cDnyilFU=',
+        channelSecret: process.env.ChannelSecret,
+        channelAccessToken: process.env.ChannelAccessToken,
         lineJwt: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoibGluZSIsImlhdCI6MTQ5NTE4MDE3OSwiZXhwIjoxNTAwMzY0MTc5fQ.D8njA7lMUnZ_Nj5tOEoK3JkwZWFu-IihanJxiYU1Yp4'
     },
 
